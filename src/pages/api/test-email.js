698bac7945be87b6d9aa899d7d76897d2b90a64d
ravymaster <rodrigo00787@hotmail.com>
2025-09-24 00:21:55 -0300

    Crear test-email.js

---
+++ src/pages/api/test-email.js
@@ -0,0 +1,11 @@
+import { sendEmail } from "@/src/lib/mailer";
+
+export default async function handler(req, res) {
+  const r = await sendEmail({
+    to: "contacto@rifex.pro",
+    subject: "Test Resend (Rifex)",
+    html: "<b>Hola desde producción</b>",
+    text: "Hola desde producción",
+  });
+  res.status(r.ok ? 200 : 500).json(r);
+}
